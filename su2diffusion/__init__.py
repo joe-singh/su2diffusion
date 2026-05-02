68f87b012b65d88e5fb260fa3104bcf2fa6afd07
Jyotirmai Singh <joesingh@stanford.edu>
2026-05-01 21:23:02 -0700

    Add forward process diagnostics

--- su2diffusion/__init__.py
+++ su2diffusion/__init__.py
@@ -4,6 +4,7 @@ from .data import BlobConfig, sample_clean_blobs
 from .diagnostics import SampleDiagnostics, diagnose_samples, print_diagnostics_table
 from .diffusion import DiffusionSchedule, brownian_forward_heat_target, heat_epsilon_target
 from .experiment import ExperimentConfig, ExperimentResult, get_experiment_config, run_experiment
+from .forward_checks import ForwardProcessDiagnostics, diagnose_forward_process, print_forward_diagnostics
 from .model import SU2Denoiser
 from .quaternion import q_exp, q_inv, q_log, q_mul, q_normalize, sample_haar, su2_distance
 from .sampling import sample_reverse, sample_reverse_trajectory
@@ -15,14 +16,17 @@ __all__ = [
     "DiffusionSchedule",
     "ExperimentConfig",
     "ExperimentResult",
+    "ForwardProcessDiagnostics",
     "SampleDiagnostics",
     "SU2Denoiser",
     "TrainConfig",
     "brownian_forward_heat_target",
     "diagnose_samples",
+    "diagnose_forward_process",
     "get_experiment_config",
     "heat_epsilon_target",
     "print_diagnostics_table",
+    "print_forward_diagnostics",
     "plot_experiment_report",
     "q_exp",
     "q_inv",
